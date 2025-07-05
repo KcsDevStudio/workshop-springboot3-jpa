f92ae156b7f61d547e280fd6ffe458aa6658fa97
Kayo César <antkayocesrdev@gmail.com>
2025-07-05 13:43:25 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -1,6 +1,5 @@
 package com.educandoweb.course.entities;
 
-import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -14,29 +13,30 @@ import jakarta.persistence.Transient;
 
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
+@Table(name = "tb_products")
+public class Product {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
 	private String name;
+	private String description;
+	private double prince;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
-	
-	public Category() {}
-	
-	
+	private Set<Category> categories = new HashSet<>();
 
-	public Category(Long id, String name) {
-		Id = id;
-		this.name = name;
+	public Product() {
 	}
 
-
+	public Product(Long id, String name, String description, double prince, String imgUrl) {
+		Id = id;
+		this.name = name;
+		this.description = description;
+		this.prince = prince;
+		this.imgUrl = imgUrl;
+	}
 
 	public Long getId() {
 		return Id;
@@ -54,8 +54,33 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
-	public Set<Product> getProducts() {
-		return products;
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrince() {
+		return prince;
+	}
+
+	public void setPrince(double prince) {
+		this.prince = prince;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
+	
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -71,9 +96,10 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(Id, other.Id);
 	}
+	
 	
 	
 	
